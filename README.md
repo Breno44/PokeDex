0c1c56dd1802d173b8eb9df0d942764e71785617
Breno Henrique <bhfs.2908@gmail.com>
2021-07-10 11:24:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">PokeDex</h1>
+
+<p align="center">🚀 Listagem de 800 pokemons <a link="https://pokeapi.co/">PokeAPI<a></p>
+
+<img src="./readme/PokeDex.png">
---
+++ script.js
@@ -0,0 +1,25 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const generatePokemonPromises = () => Array(800).fill().map((_, index) => fetch(getPokemonUrl(index + 1)).then(res => res.json()))
+
+const generateHTML = pokemons => pokemons.reduce((acc, pokemon) => {
+const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+acc += `
+    <li class="card ${types[0]}">
+        <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
+        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+        <p class="card-subtitle">${types.join(' | ')}</p>
+    </li>`
+    return acc
+}, '')    
+
+const insertPokemonIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = pokemons
+}
+
+const pokemonPromises = generatePokemonPromises()
+
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+    .then(insertPokemonIntoPage)
